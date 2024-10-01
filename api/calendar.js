f9f211d5a5438b48453038e9e5d8cfc4a333197d
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2024-10-01 12:01:11 -0700

    Update lastUpdated timestamp to be in s format instead of ms

--- api/calendar.js
+++ api/calendar.js
@@ -4,5 +4,8 @@ export const saveCalendar = (instanceRef, calendar) =>
   db
     .set(db.child(instanceRef, `/calendars`), calendar)
     .then(() =>
-      db.set(db.child(instanceRef, "/lastUpdated"), new Date().getTime()),
+      db.set(
+        db.child(instanceRef, "/lastUpdated"),
+        Math.floor(new Date().getTime() / 1000),
+      ),
     );
